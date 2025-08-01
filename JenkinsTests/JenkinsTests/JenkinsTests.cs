0a935823869754e4251bca099d70ce511ce7970d
Maryia Varankova <Maryia_Varankova@epam.com>
2025-08-01 14:38:48 +0300

    Add girst test

---
+++ JenkinsTests/JenkinsTests/JenkinsTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using FluentAssertions;
+
+namespace JenkinsTests
+{
+    [TestFixture]
+    public class JenkinsTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [Test]
+        public void JenkinsTest1()
+        {
+            driver.Navigate().GoToUrl("https://www.epam.com/");
+            driver.FindElement(By.XPath("//a[contains(@class, 'top-navigation__item-link') and contains(., 'Careers')]")).Click();
+            driver.FindElement(By.Id("new_form_job_search-keyword")).Displayed.Should().BeTrue();
+        }
+
+    }
+}
