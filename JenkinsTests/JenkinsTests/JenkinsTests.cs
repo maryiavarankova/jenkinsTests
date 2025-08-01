ee4195da7a42e1d01097ba630305d408deae2c2a
Maryia Varankova <Maryia_Varankova@epam.com>
2025-08-01 15:24:49 +0300

    update chrome options

--- JenkinsTests/JenkinsTests/JenkinsTests.cs
+++ JenkinsTests/JenkinsTests/JenkinsTests.cs
@@ -14,7 +14,14 @@ namespace JenkinsTests
         public void SetUp()
         {
             var options = new ChromeOptions();
+
+            // Provide a unique user-data-dir path for Chrome
             options.AddArgument($"--user-data-dir={Path.GetTempPath()}chrome_user_data_dir_{Guid.NewGuid()}");
+
+            // Optional: Run without GUI (headless)
+            options.AddArgument("--headless");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--no-sandbox");
             driver = new ChromeDriver(options);
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
